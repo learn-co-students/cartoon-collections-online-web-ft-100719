dccc55ac3a42627238fccc4464e342d566fa5a45
Daniel Rooker <dannyrooks@gmail.com>
2019-10-11 15:14:47 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,11 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(array)
-  array.map! {|name| name.capitalize + "!"}
+  array.map {|name| name.capitalize + "!"}
 end
 
 def long_planeteer_calls(array)
-  array.any? do |call|
-    call.length > 4
-  end 
+  array.any? {|call| call.length > 4}
 end
 
 def find_the_cheese(ingredient)
