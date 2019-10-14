7d8f38ec91179c745bdff7a0a95df34d16b065ba
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-14 03:14:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def # code an argument here
+names = ["Doc", "Dopey", "Bashful", "Grumpy"]
+names.each_with_index{|name,index|names[i]= index}
+while i += 1 > 4
+puts names.all
+end
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]# Your code here
+  while summon_captain_planet.first[0] do
+  planeteer_calls.Capitalize && << '!'{|planeteer_calls[i]|}
+  summon_captain_planet [0]+=1
 end
 
 def long_planeteer_calls# code an argument here
@@ -13,4 +20,5 @@ end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.include ("gouda")
 end
